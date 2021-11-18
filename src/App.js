684dcaf57adb570df554b1cd7010143b10245219
nty <nathan11002@gmail.com>
2021-11-18 16:00:40 +0300

    convert project to jsx, add react specific eslint config

--- src/App.js
+++ src/App.js
@@ -3,26 +3,34 @@ import { render } from 'react-dom'
 import  Pet  from './Pet';
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Boob",
-      animal: "Dog",
-      breed: "haveneese",
-    }),
-    React.createElement(Pet, {
-      name: "cat",
-      animal: "Cat",
-      breed: "sphinx",
-    }),
-    React.createElement(Pet, {
-      name: "dudit",
-      animal: "Hen",
-      breed: "Chicken",
-    })
-  );
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   React.createElement(Pet, {
+  //     name: "Boob",
+  //     animal: "Dog",
+  //     breed: "haveneese",
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "cat",
+  //     animal: "Cat",
+  //     breed: "sphinx",
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "dudit",
+  //     animal: "Hen",
+  //     breed: "Chicken",
+  //   })
+  // );
+  return(
+    <div>
+      <h1>Adopt me!</h1>
+      <Pet name="Bob" animal="dog" breed="haveneese"/>
+      <Pet name="Cat" animal="cat" breed="catter"/>
+      <Pet name="doro" animal="hen" breed="checken"/>
+    </div>
+  )
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
