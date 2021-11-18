1bcef457b2e03ccad25e8449a620a16b72ec6913
nty <nathan11002@gmail.com>
2021-11-18 22:54:33 +0300

    useState, custom hooks, update eslint, bring in pathfinder client

--- src/App.js
+++ src/App.js
@@ -1,34 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom'
-import  Pet  from './Pet';
+import  SearchParams  from './SearchParams';
+
 
 const App = () => {
-  // return React.createElement(
-  //   "div",
-  //   {},
-  //   React.createElement("h1", {}, "Adopt Me!"),
-  //   React.createElement(Pet, {
-  //     name: "Boob",
-  //     animal: "Dog",
-  //     breed: "haveneese",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "cat",
-  //     animal: "Cat",
-  //     breed: "sphinx",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "dudit",
-  //     animal: "Hen",
-  //     breed: "Chicken",
-  //   })
-  // );
   return(
     <div>
       <h1>Adopt me!</h1>
-      <Pet name="Bob" animal="dog" breed="haveneese"/>
-      <Pet name="Cat" animal="cat" breed="catter"/>
-      <Pet name="doro" animal="hen" breed="checken"/>
+      <SearchParams />
     </div>
   )
 };
